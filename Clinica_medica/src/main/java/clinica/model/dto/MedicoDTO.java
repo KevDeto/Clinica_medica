0c84d5a0a02e2581c44baf891d1a0245d2fe5f67
KevDeto <contactohuayta@gmail.com>
2025-01-21 20:53:44 -0300

    realizados los DTO de paciente y medico

--- Clinica_medica/src/main/java/clinica/model/dto/MedicoDTO.java
+++ Clinica_medica/src/main/java/clinica/model/dto/MedicoDTO.java
@@ -3,21 +3,27 @@ package clinica.model.dto;
 import java.time.LocalDate;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 public class MedicoDTO {
-	private Long id_paciente;
+	@JsonIgnore
+	private Long id;
 	private String nombre;
 	private String apellido;
 	private String dni;
-	private LocalDate fehca_nac;
+	private LocalDate fecha_nac;
 	private String email;
 	private String telefono;
 	private String direccion;
 	private String especialidad;
-//	private List<Long> cita_medica;
 	private double sueldo;
+
+//	private List<Long> cita_medica;
 }
--- Clinica_medica/src/main/java/clinica/model/dto/PacienteDTO.java
+++ Clinica_medica/src/main/java/clinica/model/dto/PacienteDTO.java
@@ -2,17 +2,22 @@ package clinica.model.dto;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 public class PacienteDTO {
-	private Long id_paciente;
+	@JsonIgnore
+	private Long id;
 	private String nombre;
 	private String apellido;
 	private String dni;
-	private LocalDate fehca_nac;
+	private LocalDate fecha_nac;
 	private String email;
 	private String telefono;
 	private String direccion;
