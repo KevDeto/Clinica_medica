776c58a966215d10a45eed548cdc07dd8bbffea5
KevDeto <contactohuayta@gmail.com>
2025-01-23 16:31:26 -0300

    creado el controller para turno

---
+++ Clinica_medica/src/main/java/clinica/controller/TurnoController.java
@@ -0,0 +1,34 @@
+package clinica.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import clinica.model.dto.TurnoDTO;
+import clinica.model.payload.MensajeResponse;
+import clinica.service.ITurnoService;
+import jakarta.validation.Valid;
+//suponiendo que el rol es del Medico
+@RestController
+@RequestMapping("api/v1/")
+public class TurnoController {
+
+	@Autowired
+	private ITurnoService turnoService;
+	
+	@PostMapping("horario-atencion/{medico_id}")
+	public ResponseEntity<MensajeResponse> crear(@Valid@PathVariable Long medico_id,
+			@Valid@RequestBody TurnoDTO turno){
+		TurnoDTO turnoCreado = turnoService.crearHorarioAtencion(medico_id, turno);
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(MensajeResponse.builder()
+						.mensaje("Horario de atencion creado correctamente.")
+						.objeto(turnoCreado)
+						.build());
+	}
+}
