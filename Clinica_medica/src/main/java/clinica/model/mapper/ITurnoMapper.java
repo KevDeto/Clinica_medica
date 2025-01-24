8c055567ea13ef716b241bf7ad5649d56491a73b
KevDeto <contactohuayta@gmail.com>
2025-01-24 15:54:58 -0300

    configurado para mapear al medico y se elimino el INSTANCE

--- Clinica_medica/src/main/java/clinica/model/mapper/ITurnoMapper.java
+++ Clinica_medica/src/main/java/clinica/model/mapper/ITurnoMapper.java
@@ -1,15 +1,36 @@
 package clinica.model.mapper;
 
-
+import org.mapstruct.Context;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
+import clinica.exceptions.ApplicationException;
+import clinica.exceptions.ErrorCode;
+import clinica.model.dao.IMedicoRepository;
 import clinica.model.dto.TurnoDTO;
+import clinica.model.entity.Medico;
 import clinica.model.entity.Turno;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = IMedicoRepository.class)
 public interface ITurnoMapper {
-	ITurnoMapper INSTANCE = Mappers.getMapper(ITurnoMapper.class);
-	Turno deTurnoDTOATurno(TurnoDTO turnoDTO);
-	TurnoDTO deTurnoATurnoDTO(Turno turno);
+//	ITurnoMapper INSTANCE = Mappers.getMapper(ITurnoMapper.class);
+    @Mapping(source = "medico_id", target = "medico", qualifiedByName = "mapearMedicoIdAMedico")
+    Turno deTurnoDTOATurno(TurnoDTO turnoDTO, @Context IMedicoRepository medicoRepository);
+
+    @Mapping(source = "medico.id", target = "medico_id")
+    TurnoDTO deTurnoATurnoDTO(Turno turno);
+
+    @Named("mapearMedicoIdAMedico")
+    default Medico mapearMedicoIdAMedico(Long medico_id, @Context IMedicoRepository medicoRepository) {
+        if (medico_id == null) {
+            return null;
+        }
+        return medicoRepository.findById(medico_id)
+                .orElseThrow(() -> new ApplicationException(ErrorCode.NOT_FOUND,
+                		"El medico con ID " + medico_id + " no existe.", null));
+    }
 }
+
+
+
