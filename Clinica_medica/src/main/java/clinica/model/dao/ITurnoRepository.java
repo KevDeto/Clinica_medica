062a51bcc8611e73a7d87369e56bf0aa12594cd8
KevDeto <contactohuayta@gmail.com>
2025-01-24 15:55:24 -0300

    sin cambios

--- Clinica_medica/src/main/java/clinica/model/dao/ITurnoRepository.java
+++ Clinica_medica/src/main/java/clinica/model/dao/ITurnoRepository.java
@@ -1,11 +1,16 @@
 package clinica.model.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import clinica.model.entity.Turno;
 
 @Repository
 public interface ITurnoRepository extends JpaRepository<Turno, Long>{
-
+    List<Turno> findByMedicoId(Long medicoId);
 }
--- Clinica_medica/src/main/java/clinica/model/dto/TurnoDTO.java
+++ Clinica_medica/src/main/java/clinica/model/dto/TurnoDTO.java
@@ -1,13 +1,13 @@
 package clinica.model.dto;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import clinica.model.entity.EstadoTurno;
-import jakarta.validation.constraints.Pattern;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,16 +17,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class TurnoDTO {
+public class TurnoDTO implements Serializable{
 	@JsonIgnore
-	private Long id;
-    @JsonFormat(pattern = "yyyy-MM-dd")
-	private LocalDate fecha;
-    @JsonFormat(pattern = "HH:mm:ss")
-	private LocalTime hora_inicio;
-    @JsonFormat(pattern = "HH:mm:ss")
-	private LocalTime hora_fin;
-	@JsonIgnore
-	private Long medico_id;
-	private EstadoTurno estado;
+    private Long id;
+//    @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
+    private LocalDate fecha;
+//    @JsonFormat(pattern = "HH:mm", shape = JsonFormat.Shape.STRING)
+    @Schema(example = "08:00")
+    private LocalTime hora_inicio;
+//    @JsonFormat(pattern = "HH:mm", shape = JsonFormat.Shape.STRING)
+    @Schema(example = "12:00")
+    private LocalTime hora_fin;
+    @Schema(example = "0")
+    private Long medico_id;
 }
+
