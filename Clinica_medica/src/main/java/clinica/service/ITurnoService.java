3c4a9df6a9c620551448adeee59ee69953fb54d2
KevDeto <contactohuayta@gmail.com>
2025-01-24 15:56:23 -0300

    sin cambios relevantes

--- Clinica_medica/src/main/java/clinica/service/ITurnoService.java
+++ Clinica_medica/src/main/java/clinica/service/ITurnoService.java
@@ -3,5 +3,5 @@ package clinica.service;
 import clinica.model.dto.TurnoDTO;
 
 public interface ITurnoService {
-	TurnoDTO crearHorarioAtencion(Long medico_id, TurnoDTO turnoDTO);
+	TurnoDTO crearTurno(TurnoDTO turnoDTO);
 }
