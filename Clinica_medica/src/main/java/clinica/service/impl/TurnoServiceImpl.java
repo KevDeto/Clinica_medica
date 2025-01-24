a51f9d00533e07d957cfa4e5e53ae8c1903c1c52
KevDeto <contactohuayta@gmail.com>
2025-01-24 15:55:53 -0300

    se modifico la logica del mapper

--- Clinica_medica/src/main/java/clinica/service/impl/TurnoServiceImpl.java
+++ Clinica_medica/src/main/java/clinica/service/impl/TurnoServiceImpl.java
@@ -1,17 +1,15 @@
 package clinica.service.impl;
 
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
-
+import org.mapstruct.Context;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import clinica.exceptions.ApplicationException;
 import clinica.exceptions.ErrorCode;
+import clinica.model.dao.IMedicoRepository;
 import clinica.model.dao.ITurnoRepository;
 import clinica.model.dto.TurnoDTO;
-import clinica.model.entity.EstadoTurno;
+import clinica.model.entity.Medico;
 import clinica.model.entity.Turno;
 import clinica.model.mapper.ITurnoMapper;
 import clinica.service.ITurnoService;
@@ -21,27 +19,21 @@ public class TurnoServiceImpl implements ITurnoService{
 	
 	@Autowired
 	private ITurnoRepository turnoRepository;
+	@Autowired
+	private IMedicoRepository medicoRepository;
+    @Autowired
+    private ITurnoMapper turnoMapper;
 
 	@Override
-	public TurnoDTO crearHorarioAtencion(Long medico_id, TurnoDTO turnoDTO) {
+	public TurnoDTO crearTurno(TurnoDTO turnoDTO) {
 	    if (turnoDTO.getHora_inicio().isAfter(turnoDTO.getHora_fin())) {
 	        throw new ApplicationException(ErrorCode.BAD_REQUEST,
 	        		"La hora de inicio no puede ser después de la hora de fin", null);
 	    }
 	    try {
-	        for (LocalTime hora = turnoDTO.getHora_inicio(); hora.isBefore(turnoDTO.getHora_fin()); hora = hora.plusMinutes(5)) {
-	            TurnoDTO turnoCreado = new TurnoDTO();
-	            turnoCreado.setFecha(turnoDTO.getFecha());
-	            turnoCreado.setHora_inicio(turnoDTO.getHora_inicio());
-	            turnoCreado.setHora_fin(hora.plusMinutes(5));
-	            turnoCreado.setMedico_id(medico_id);
-	            turnoCreado.setEstado(EstadoTurno.DISPONIBLE);
-	            
-	            Turno mapperTurno = ITurnoMapper.INSTANCE.deTurnoDTOATurno(turnoDTO);
-	            
-	            turnoRepository.save(mapperTurno);
-	        }
-			return turnoDTO;
+	    	System.out.println(turnoDTO.toString());
+	        Turno mapperTurno = turnoMapper.deTurnoDTOATurno(turnoDTO, medicoRepository);
+	        return turnoMapper.deTurnoATurnoDTO(turnoRepository.save(mapperTurno));
 	    }catch(Exception ex) {
 	        throw new ApplicationException(ErrorCode.INTERNAL_SERVER_ERROR, 
 	                "Error desconocido al guardar el horario disponible.", ex);
