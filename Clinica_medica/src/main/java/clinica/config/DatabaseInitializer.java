a5f11de1e543903a1d59d694e389e5f194dd4611
KevDeto <contactohuayta@gmail.com>
2025-01-24 15:58:45 -0300

    se agrego para eliminar los nanosegundos en la base de datos (provisorio?)

---
+++ Clinica_medica/src/main/java/clinica/config/DatabaseInitializer.java
@@ -0,0 +1,31 @@
+package clinica.config;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseInitializer {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void initializeDatabase() {
+		try (Connection connection = dataSource.getConnection(); Statement statement = connection.createStatement()) {
+
+			String alterQuery = "ALTER TABLE turno " + "MODIFY COLUMN hora_inicio TIME(0), "
+					+ "MODIFY COLUMN hora_fin TIME(0)";
+			statement.execute(alterQuery);
+		} catch (SQLException ex) {
+			throw new RuntimeException("Error ejecutando consulta SQL para modificar la tabla 'turno'", ex);
+		}
+	}
+}
