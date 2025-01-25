7764b9d9586be18baea005fba5d28d16de25b52b
KevDeto <contactohuayta@gmail.com>
2025-01-25 20:15:04 -0300

    realizado los metodos service

--- Clinica_medica/src/main/java/clinica/service/ITurnoService.java
+++ Clinica_medica/src/main/java/clinica/service/ITurnoService.java
@@ -1,7 +1,13 @@
 package clinica.service;
 
+import java.util.List;
+
 import clinica.model.dto.TurnoDTO;
 
 public interface ITurnoService {
 	TurnoDTO crearTurno(TurnoDTO turnoDTO);
+	TurnoDTO obtenerTurno(Long id);
+	TurnoDTO actualizarTurno(Long id, TurnoDTO turnoDTO);
+	void eliminarTurno(Long id);
+	List<TurnoDTO> listaTurnos();
 }
