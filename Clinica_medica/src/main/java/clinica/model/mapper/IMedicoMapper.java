d7f188af0ea8deb4a53175aaac57a0aaec0821fd
KevDeto <contactohuayta@gmail.com>
2025-01-28 15:22:39 -0300

    termine de modificar los mappers

--- Clinica_medica/src/main/java/clinica/model/mapper/IMedicoMapper.java
+++ Clinica_medica/src/main/java/clinica/model/mapper/IMedicoMapper.java
@@ -1,38 +1,21 @@
 package clinica.model.mapper;
 
-import java.util.List;
-
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import clinica.model.dto.MedicoDTO;
 import clinica.model.entity.Medico;
-//utilizar un mapper personalisado para que no choque entre IDs
-//revisar el mapeo de turnos
-//@Mapper(componentModel = "spring", uses = ITurnoMapper.class)
+
 @Mapper(componentModel = "spring", uses = ITurnoMapper.class)
 public interface IMedicoMapper {
+	@Mapping(target = "turnos", ignore = true)
+	@Mapping(target = "id", ignore = true)
+	Medico deMedicoDTOAMedico(MedicoDTO medicoDTO);
 	@Mapping(source = "turnos", target = "turnos")
 	@Mapping(source = "id", target = "id")
 	MedicoDTO deMedicoAMedicoDTO(Medico medico);
-	@Mapping(source = "turnos", target = "turnos")
-	@Mapping(target = "id", ignore = true)
-	Medico deMedicoDTOAMedico(MedicoDTO medicoDTO);
 	@Mapping(target = "turnos", ignore = true)
-	@Mapping(source = "id", target = "id")
+	@Mapping(target = "id", ignore = true)
 	void actualizarDeMedicoAMedicoDTO(@MappingTarget Medico medico, MedicoDTO medicoDTO);
-
-//	@Mapping(target = "turnos", source = "turnos")
-//    @Mapping(target = "id", ignore = true)
-//	MedicoDTO deMedicoAMedicoDTO(Medico medico);
-//	@Mapping(target = "turnos", source = "turnos")
-//    @Mapping(target = "id", ignore = true)
-//	Medico deMedicoDTOAMedico(MedicoDTO medicoDTO);
-//	@Mapping(target = "turnos", ignore = true)
-//    @Mapping(target = "id", source = "id")
-//	void actualizarMedicoDesdeDTO(@MappingTarget Medico medico, MedicoDTO medicoDTO);
-//	
-//	List<MedicoDTO> deListaMedicoAListaMedicoDTO(List<Medico> listaMedico);
-////	List<Medico> deListaMedicoDTOAListaMedico(List<MedicoDTO> listaMedicoDTO);
 }
--- Clinica_medica/src/main/java/clinica/model/mapper/ITurnoMapper.java
+++ Clinica_medica/src/main/java/clinica/model/mapper/ITurnoMapper.java
@@ -1,38 +1,31 @@
 package clinica.model.mapper;
 
-import java.util.List;
-
-import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.Named;
 
-import clinica.exceptions.ApplicationException;
-import clinica.exceptions.ErrorCode;
-import clinica.model.dao.IMedicoRepository;
 import clinica.model.dto.TurnoDTO;
 import clinica.model.entity.Medico;
 import clinica.model.entity.Turno;
 
-//@Mapper(componentModel = "spring", uses = IMedicoRepository.class)
 @Mapper(componentModel = "spring")
 public interface ITurnoMapper {
 	@Mapping(source = "medico_id", target = "medico", qualifiedByName = "mapMedicoIdToMedico")
 	@Mapping(target = "id", ignore = true)
 	Turno deTurnoDTOATurno(TurnoDTO turnoDTO);
 	@Mapping(source = "medico", target = "medico_id", qualifiedByName = "mapMedicoToMedicoId")
-	@Mapping(target = "id", ignore = true)
+	@Mapping(source = "id", target = "id")
 	TurnoDTO deTurnoATurnoDTO(Turno turno);
 	@Mapping(source = "medico_id", target = "medico.id")
-	@Mapping(target = "id", source = "id")
+	@Mapping(target = "id", ignore = true)
 	void actualizarDeTurnoATurnoDTO(@MappingTarget Turno turno, TurnoDTO turnoDTO);
 
     @Named("mapMedicoIdToMedico")
     default Medico mapMedicoIdToMedico(Long medico_id) {
-        if (medico_id == null) {
-            return null;
-        }
+//        if (medico_id == null) {
+//            return null;
+//        }
         Medico medico = new Medico();
         medico.setId(medico_id);
         return medico;
@@ -40,27 +33,9 @@ public interface ITurnoMapper {
 
     @Named("mapMedicoToMedicoId")
     default Long mapMedicoToMedicoId(Medico medico) {
-        if (medico == null) {
-            return null;
-        }
+//        if (medico == null) {
+//            return null;
+//        }
         return medico.getId();
     }
-//    @Mapping(source = "medico_id", target = "medico", qualifiedByName = "mapearMedicoIdAMedico")
-//    Turno deTurnoDTOATurno(TurnoDTO turnoDisponibleDTO, @Context IMedicoRepository medicoRepository);
-//
-//    @Mapping(source = "medico.id", target = "medico_id")
-//    TurnoDTO deTurnoATurnoDTO(Turno turno);
-//
-//    @Mapping(source = "medico_id", target = "medico.id")
-//    @Mapping(source = "id", target = "id")
-//    void actualizarTurnoDesdeDTO(@MappingTarget Turno turno, TurnoDTO turnoDTO);
-//
-//    List<TurnoDTO> deListaTurnoAListaTurnoDTO(List<Turno> listaTurno);
-//
-//    List<Turno> deListaTurnoDTOAListaTurno(List<TurnoDTO> listaTurnoDTO, @Context IMedicoRepository medicoRepository);
-//
-//    @Named("mapearMedicoIdAMedico")
-//    default Medico mapearMedicoIdAMedico(Long medico_id, @Context IMedicoRepository medicoRepository) {
-//        return medicoRepository.getReferenceById(medico_id);
-//    }
 }
