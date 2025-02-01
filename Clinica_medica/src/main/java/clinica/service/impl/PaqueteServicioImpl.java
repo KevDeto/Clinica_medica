2dc34eed768ce781d96d8d68abfcce9ed4acf436
KevDeto <contactohuayta@gmail.com>
2025-02-01 16:23:10 -0300

    se creo la clase service impl junto con sus metodos service

---
+++ Clinica_medica/src/main/java/clinica/service/impl/PaqueteServicioImpl.java
@@ -0,0 +1,40 @@
+package clinica.service.impl;
+
+import java.util.List;
+
+import clinica.model.dto.PaqueteServicioDTO;
+import clinica.service.IPaqueteServicioService;
+
+public class PaqueteServicioImpl implements IPaqueteServicioService{
+
+	@Override
+	public PaqueteServicioDTO crearPaqueteServicio(PaqueteServicioDTO paqueteServicioDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PaqueteServicioDTO obtenerPaqueteServicio(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PaqueteServicioDTO actualizarPaqueteServicio(Long id, PaqueteServicioDTO paqueteServicioDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eliminarPaqueteServicio(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<PaqueteServicioDTO> listaPaqueteServicio() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Clinica_medica/src/main/java/clinica/service/impl/ServicioMedicoImpl.java
@@ -0,0 +1,40 @@
+package clinica.service.impl;
+
+import java.util.List;
+
+import clinica.model.dto.ServicioMedicoDTO;
+import clinica.service.IServicioMedicoService;
+
+public class ServicioMedicoImpl implements IServicioMedicoService{
+
+	@Override
+	public ServicioMedicoDTO crearServicioMedico(ServicioMedicoDTO servicioMedicoDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ServicioMedicoDTO obtenerServicioMedico(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ServicioMedicoDTO actualizarServicioMedico(Long id, ServicioMedicoDTO servicioMedicoDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eliminarServicioMedico(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<ServicioMedicoDTO> listaServicioMedico() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
