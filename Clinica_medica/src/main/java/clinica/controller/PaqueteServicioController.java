55c101bae7e94d26c57e7e045a26573375320fe6
KevDeto <contactohuayta@gmail.com>
2025-02-01 16:21:13 -0300

    terminada la implementacion de controllers de servicio y paquete

---
+++ Clinica_medica/src/main/java/clinica/controller/PaqueteServicioController.java
@@ -0,0 +1,74 @@
+package clinica.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import clinica.model.dto.PaqueteServicioDTO;
+import clinica.model.dto.ServicioMedicoDTO;
+import clinica.model.payload.MensajeResponse;
+import clinica.service.IPaqueteServicioService;
+import clinica.service.IServicioMedicoService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1")
+public class PaqueteServicioController {
+
+	@Autowired
+	private IPaqueteServicioService paqueteServicioService;
+	
+	@GetMapping("/paquete")
+	public ResponseEntity<MensajeResponse> crear(@Valid@RequestBody PaqueteServicioDTO paqueteServicioDTO){
+		PaqueteServicioDTO paqueteServicio = paqueteServicioService.crearPaqueteServicio(paqueteServicioDTO);
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(MensajeResponse.builder()
+						.mensaje("Paquete registrado correctamente.")
+						.objeto(paqueteServicio)
+						.build());
+	}
+	
+	@GetMapping("/paquete/{id}")
+	public ResponseEntity<MensajeResponse> obtener(@Valid@PathVariable Long id){
+		PaqueteServicioDTO paqueteServicio = paqueteServicioService.obtenerPaqueteServicio(id);
+		return ResponseEntity.ok(MensajeResponse.builder()
+				.mensaje("Paquete recuperado correctamente.")
+				.objeto(paqueteServicio)
+				.build());
+	}
+	
+	@PutMapping("/paquete/{id}")
+	public ResponseEntity<MensajeResponse> actualizar(@Valid@PathVariable Long id,
+			@Valid@RequestBody PaqueteServicioDTO paqueteServicioDTO){
+		PaqueteServicioDTO paqueteServicio = paqueteServicioService.actualizarPaqueteServicio(id, paqueteServicioDTO);
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(MensajeResponse.builder()
+						.mensaje("Paquete actualizado correctamente.")
+						.objeto(paqueteServicio)
+						.build());
+	}
+	
+	@DeleteMapping("/paquete/{id}")
+	public ResponseEntity<MensajeResponse> eliminar(@Valid@PathVariable Long id){
+		paqueteServicioService.eliminarPaqueteServicio(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
+	
+	@GetMapping("/paquetes")
+	public ResponseEntity<MensajeResponse> obtenerTodos(){
+		List<PaqueteServicioDTO> listarPaquetes = paqueteServicioService.listaPaqueteServicio();
+		return ResponseEntity.ok(MensajeResponse.builder()
+				.mensaje("Paquetes recuperados correctamente.")
+				.objeto(listarPaquetes)
+				.build());
+	}
+}
---
+++ Clinica_medica/src/main/java/clinica/controller/ServicioMedicoController.java
@@ -0,0 +1,72 @@
+package clinica.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import clinica.model.dto.ServicioMedicoDTO;
+import clinica.model.payload.MensajeResponse;
+import clinica.service.IServicioMedicoService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ServicioMedicoController {
+
+	@Autowired
+	private IServicioMedicoService servicioMedicoService;
+	
+	@GetMapping("/servicio")
+	public ResponseEntity<MensajeResponse> crear(@Valid@RequestBody ServicioMedicoDTO servicioMedicoDTO){
+		ServicioMedicoDTO servicioMedico = servicioMedicoService.crearServicioMedico(servicioMedicoDTO);
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(MensajeResponse.builder()
+						.mensaje("Servicio medico registrado correctamente.")
+						.objeto(servicioMedico)
+						.build());
+	}
+	
+	@GetMapping("/servicio/{id}")
+	public ResponseEntity<MensajeResponse> obtener(@Valid@PathVariable Long id){
+		ServicioMedicoDTO servicioMedico = servicioMedicoService.obtenerServicioMedico(id);
+		return ResponseEntity.ok(MensajeResponse.builder()
+				.mensaje("Servicio medico recuperado correctamente.")
+				.objeto(servicioMedico)
+				.build());
+	}
+	
+	@PutMapping("/servicio/{id}")
+	public ResponseEntity<MensajeResponse> actualizar(@Valid@PathVariable Long id,
+			@Valid@RequestBody ServicioMedicoDTO servicioMedicoDTO){
+		ServicioMedicoDTO servicioMedico = servicioMedicoService.actualizarServicioMedico(id, servicioMedicoDTO);
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(MensajeResponse.builder()
+						.mensaje("Servicio medico actualizado correctamente.")
+						.objeto(servicioMedico)
+						.build());
+	}
+	
+	@DeleteMapping("/servicio/{id}")
+	public ResponseEntity<MensajeResponse> eliminar(@Valid@PathVariable Long id){
+		servicioMedicoService.eliminarServicioMedico(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
+	
+	@GetMapping("/servicios")
+	public ResponseEntity<MensajeResponse> obtenerTodos(){
+		List<ServicioMedicoDTO> listaServiciosMedicos = servicioMedicoService.listaServicioMedico();
+		return ResponseEntity.ok(MensajeResponse.builder()
+				.mensaje("Servicios medicos recuperados correctamente.")
+				.objeto(listaServiciosMedicos)
+				.build());
+	}
+}
