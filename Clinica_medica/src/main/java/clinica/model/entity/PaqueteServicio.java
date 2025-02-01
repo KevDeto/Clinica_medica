c6bc551e8e5b556c945aeb80c63f6e818a7b4182
KevDeto <contactohuayta@gmail.com>
2025-02-01 16:25:31 -0300

    se crearon las entidades servicio y paquete

--- Clinica_medica/src/main/java/clinica/model/entity/PaqueteServicio.java
+++ Clinica_medica/src/main/java/clinica/model/entity/PaqueteServicio.java
@@ -1,5 +1,35 @@
 package clinica.model.entity;
 
-public class PaqueteServicio {
+import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "paquetes")
+public class PaqueteServicio {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigoPaquete;
+	private double precio;
+	@ManyToMany
+	@JoinTable(
+			name = "paquete_servicio",
+			joinColumns = @JoinColumn(name = "paquete_id"),
+			inverseJoinColumns = @JoinColumn(name = "servicio_id"))
+	private List<ServicioMedico> listaServicios;
 }
--- Clinica_medica/src/main/java/clinica/model/entity/ServicioMedico.java
+++ Clinica_medica/src/main/java/clinica/model/entity/ServicioMedico.java
@@ -1,5 +1,37 @@
 package clinica.model.entity;
 
-public class ServicioMedico {
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "servicios")
+public class ServicioMedico {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long codigoServicio;
+	private String nombre;
+	private String descripcion;
+	private double precio;
+	
+	@ManyToMany(mappedBy = "listaServicios", cascade = CascadeType.ALL)
+	private List<PaqueteServicio> listaPaquetes;
+
+	@OneToMany(mappedBy = "citaMedica", cascade = CascadeType.ALL)
+	private List<CitaMedica> citaMedica;
 }
