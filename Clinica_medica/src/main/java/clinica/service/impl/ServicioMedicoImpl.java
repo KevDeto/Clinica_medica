18a3e4e55346dcbc3105b0f0f54ef78c1de5e816
KevDeto <contactohuayta@gmail.com>
2025-02-03 13:25:49 -0300

    realizada la logica de los metodos service en servicio medico

--- Clinica_medica/src/main/java/clinica/service/impl/ServicioMedicoImpl.java
+++ Clinica_medica/src/main/java/clinica/service/impl/ServicioMedicoImpl.java
@@ -1,40 +1,96 @@
 package clinica.service.impl;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import clinica.exceptions.ApplicationException;
+import clinica.exceptions.ErrorCode;
+import clinica.model.dao.IServicioMedicoRepository;
 import clinica.model.dto.ServicioMedicoDTO;
+import clinica.model.entity.ServicioMedico;
+import clinica.model.mapper.IServicioMapper;
 import clinica.service.IServicioMedicoService;
+import lombok.RequiredArgsConstructor;
 
-public class ServicioMedicoImpl implements IServicioMedicoService{
+@Service
+@RequiredArgsConstructor
+public class ServicioMedicoImpl implements IServicioMedicoService {
 
+	private IServicioMedicoRepository servicioMedicoRepository;
+	private IServicioMapper servicioMapper;
+
+	@Transactional
 	@Override
 	public ServicioMedicoDTO crearServicioMedico(ServicioMedicoDTO servicioMedicoDTO) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+
+			ServicioMedico servicioMapeado = servicioMapper.deServicioMedicoDTOAServicioMedico(servicioMedicoDTO);
+			servicioMedicoRepository.save(servicioMapeado);
+			ServicioMedicoDTO servicioDtoMapeado = servicioMapper.deServicioMedicoAServicioMedicoDTO(servicioMapeado);
+
+			return servicioDtoMapeado;
+		} catch (DataIntegrityViolationException ex) {
+			throw new ApplicationException(ErrorCode.CONFLICT, "Error de integridad de datos: existen duplicados.", ex);
+		} catch (Exception ex) {
+			throw new ApplicationException(ErrorCode.INTERNAL_SERVER_ERROR,
+					"Error desconocido al guardar el paquete medico.", ex);
+		}
 	}
 
 	@Override
 	public ServicioMedicoDTO obtenerServicioMedico(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<ServicioMedico> servicioMedicoObtenido = servicioMedicoRepository.findById(id);
+		
+		if(servicioMedicoObtenido.isEmpty()) {
+			throw new ApplicationException(ErrorCode.NOT_FOUND,
+					"Servicio con ID " + id + " no existe.", null);
+		}
+		ServicioMedico servicio = servicioMedicoObtenido.get();
+		ServicioMedicoDTO servicioDtoMapeado = servicioMapper.deServicioMedicoAServicioMedicoDTO(servicio);
+		return servicioDtoMapeado;
 	}
 
+	@Transactional
 	@Override
 	public ServicioMedicoDTO actualizarServicioMedico(Long id, ServicioMedicoDTO servicioMedicoDTO) {
-		// TODO Auto-generated method stub
-		return null;
+		ServicioMedico servicioMedico = servicioMedicoRepository.findById(id)
+	            .orElseThrow(() -> new ApplicationException(ErrorCode.NOT_FOUND,
+	                    "Servicio con ID " + id + " no encontrado.", null));
+		try {
+			servicioMedicoDTO.setCodigoServicio(id);
+			servicioMapper.actualizarServicioMedicoDesdeDTO(servicioMedico, servicioMedicoDTO);
+			servicioMedicoRepository.save(servicioMedico);
+			ServicioMedicoDTO servicioDtoMapeado = servicioMapper.deServicioMedicoAServicioMedicoDTO(servicioMedico);
+			return servicioDtoMapeado;
+	    } catch (DataIntegrityViolationException ex) {
+	        throw new ApplicationException(ErrorCode.CONFLICT, 
+	                "Error de integridad de datos: existen duplicados.", ex);
+		} catch (Exception ex) {
+	        throw new ApplicationException(ErrorCode.INTERNAL_SERVER_ERROR, 
+	                "Error interno del servidor.", ex);
+	    }
 	}
 
 	@Override
 	public void eliminarServicioMedico(Long id) {
-		// TODO Auto-generated method stub
-		
+		if(!servicioMedicoRepository.existsById(id)) {
+			throw new ApplicationException(ErrorCode.NOT_FOUND,
+					"Servicio con ID " + id + " no existe.", null);
+		}
+		servicioMedicoRepository.deleteById(id);
 	}
 
 	@Override
 	public List<ServicioMedicoDTO> listaServicioMedico() {
-		// TODO Auto-generated method stub
-		return null;
+		List<ServicioMedico> listaPaquetes = servicioMedicoRepository.findAll();
+        return listaPaquetes.stream()
+                .map(servicioMapper::deServicioMedicoAServicioMedicoDTO)
+                .collect(Collectors.toList());
 	}
 
 }
