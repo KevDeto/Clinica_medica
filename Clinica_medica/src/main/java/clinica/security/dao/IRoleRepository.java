c1ae3bb0c6f9627d9b546d8341d5c4e89031a5d4
KevDeto <contactohuayta@gmail.com>
2025-02-08 13:00:15 -0300

    se creo el repository de rol

---
+++ Clinica_medica/src/main/java/clinica/security/dao/IRoleRepository.java
@@ -0,0 +1,12 @@
+package clinica.security.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import clinica.security.entity.RolEntity;
+
+
+@Repository
+public interface IRoleRepository extends JpaRepository<RolEntity, Long>{
+
+}
